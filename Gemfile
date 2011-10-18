c84cc54cf413cd3bdd67014eed339fae83d6273b
Collin Reisdorf <creisdorf@toth.com>
2011-10-18 12:17:50 -0400

    started to work, then abandoned it

---
+++ Gemfile
@@ -0,0 +1,17 @@
+# A sample Gemfile
+source "http://rubygems.org"
+
+# gem "rails"
+gem "sinatra"
+gem "haml"
+gem "fb_graph"
+
+group :test, :development do
+  gem "sin"
+end
+
+group :test do
+  gem "rspec"
+  gem "faker"
+end
+  
--- app.rb
+++ app.rb
@@ -1,6 +1,12 @@
 require 'rubygems'
 require 'sinatra'
 require 'haml'
+require 'open-uri'
+
+@app_id = "YOUR_APP_ID";
+@app_secret = "YOUR_APP_SECRET";
+@post_login_url = "/";
+
 
 error do
   e = request.env['sinatra.error']
@@ -14,5 +20,21 @@ end
 
 # root page
 get '/' do
+  
   haml :index
+end
+
+get '/tester' do
+  url = "https://graph.facebook.com/19292868552"
+  open(url) do |f|
+    f.each_line do |l|
+      puts "I Got #{@l}"
+    end
+  end
+  
+  haml :tester
+end
+
+get '/sign_in' do
+  
 end
