d7ff0345a33123d7e0b89db45baaaa5f98461ff4
Collin Reisdorf <creisdorf@toth.com>
2012-02-01 15:29:07 -0500

    cleaned up the basic static sinatra setup files

--- Gemfile
+++ Gemfile
@@ -2,3 +2,7 @@ source :rubygems
 gem 'sinatra'
 gem 'thin'
 gem 'haml'
+
+group :development do
+  gem "shotgun" # Feel free to change this one, I just like shotgun for launching sinatra apps.
+end
--- spawn.heroku.example.sh
+++ spawn.heroku.example.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
-# APPNAME = 'myapp'
-
-# Install Heroku gem
-sudo gem install heroku
+# You'll need the heroku gem and GIT installed to do this.
 
 # Create a heroku app
-heroku create $APPNAME
+heroku create
 
 # Push master branch to heroku
 git push heroku master
 
 # Show your newly spawned site in the browser if you 
 heroku open
-
-# If you have your domain setup use this line to open the site
-# open "http://$APPNAME.com"
