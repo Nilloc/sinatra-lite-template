37a5907ecbe911acfddcaea47d44ea26c2b27b50
Collin Reisdorf <creisdorf@toth.com>
2012-02-01 14:03:03 -0500

    slight cleanup of spawn script

--- spawn.heroku.example.sh
+++ spawn.heroku.example.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-APPNAME = 'Testerino'
+# APPNAME = 'myapp'
 
 # Install Heroku gem
 sudo gem install heroku
@@ -8,14 +8,11 @@ sudo gem install heroku
 # Create a heroku app
 heroku create $APPNAME
 
-# Add your custom domain // comment this out if tou don't want to hook up your domain yet
-heroku domains:add "$APPNAME.com"
-
 # Push master branch to heroku
 git push heroku master
 
 # Show your newly spawned site in the browser if you 
-open "http://$APPNAME.heroku.com"
+heroku open
 
 # If you have your domain setup use this line to open the site
 # open "http://$APPNAME.com"
